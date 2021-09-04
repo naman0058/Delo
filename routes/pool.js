e3dec453a6ef06008fe22fcfe7119a5c0a46b97b
naman0058 <jnaman345@gmail.com>
2021-09-05 00:13:56 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -3,11 +3,11 @@ var mysql = require('mysql')
 
 const pool = mysql.createPool({
 //host: 'localhost',
-  host:'142.93.216.211',
+  host:'167.71.231.201',
     user: 'root',
 //  password: '123',
- password:'deloservices',
- database: 'delo_services',
+ password:'123a@8Anmanraspaa123a@*Anmanraspaa',
+ database: 'delo',
     database : 'compress_schema',
     port:'3306' ,
     connectionLimit:100,
